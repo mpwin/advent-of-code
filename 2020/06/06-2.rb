4d22dc06be3bde1ff527eb0ae1bf4d544b31ddde
Matthew Winiarczyk <matthew.winiarczyk@gmail.com>
2020-12-08 16:56:37 -0500

    Add day 6, 2020

---
+++ 2020/06/06-2.rb
@@ -0,0 +1,15 @@
+require 'set'
+
+group = Set.new
+count = 0
+
+(File.readlines('input.txt') << "\n").each do |line|
+  if line == "\n"
+    count += group.inject(:&).size
+    group.clear
+  else
+    group << line.chomp.chars
+  end
+end
+
+puts count
