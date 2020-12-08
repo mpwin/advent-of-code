9b218f7676bd3acc3b07eef463b4da81bd5efc41
Matthew Winiarczyk <matthew.winiarczyk@gmail.com>
2020-12-08 17:13:12 -0500

    Edit day 5, 2020

--- 2020/05/05-1.rb
+++ 2020/05/05-1.rb
@@ -1,21 +1,9 @@
 high = 0
 
-def search(range, input)
-  while input.any?
-    mid   = range.to_a[range.size / 2]
-    range = case input.shift
-    when 'F', 'L' then range.first..mid-1
-    when 'B', 'R' then mid..range.last
-    end
-  end
-
-  return range.first
-end
-
 File.foreach('input.txt') do |line|
   r, c = line.match(/^([F|B]{7})([L|R]{3})$/).captures
-  row  = search(0..127, r.chars)
-  col  = search(0..7,   c.chars)
+  row  = r.gsub(/[FB]/, 'F' => 0, 'B' => 1).to_i(2)
+  col  = c.gsub(/[LR]/, 'L' => 0, 'R' => 1).to_i(2)
   seat = (row * 8) + col
   
   high = seat if seat > high
