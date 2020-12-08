4b1cb625f3075c0caa3123d5068e58c70e142c8f
Matthew Winiarczyk <matthew.winiarczyk@gmail.com>
2020-12-08 17:17:00 -0500

    Edit day 2, 2020

--- 2020/02/02-1.rb
+++ 2020/02/02-1.rb
@@ -1,6 +1,6 @@
 valid = 0
 
-File.open('input.txt').each do |line|
+File.foreach('input.txt') do |line|
   l, m, char, pass = line.scan(/(\d+)-(\d+) (\D): (\D+)/).flatten
 
   valid += 1 if pass.count(char).between?(l.to_i, m.to_i)
--- 2020/02/02-2.rb
+++ 2020/02/02-2.rb
@@ -1,6 +1,6 @@
 valid = 0
 
-File.open('input.txt').each do |line|
+File.foreach('input.txt') do |line|
   p1, p2, char, pass = line.scan(/(\d+)-(\d+) (\D): (\D+)/).flatten
 
   valid += 1 if (char == pass[p1.to_i-1]) ^ (char == pass[p2.to_i-1])
