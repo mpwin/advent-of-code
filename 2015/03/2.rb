10e1d4467ae5ed52c3bd819a2712d5d004ae5c4d
Matthew Winiarczyk <matthew.winiarczyk@gmail.com>
2023-02-23 18:00:49 -0500

    Clean up day 3, 2015

--- 2015/03/2.rb
+++ 2015/03/2.rb
@@ -1,12 +1,10 @@
-require 'set'
-
 santas = [0i, 0i]
 houses = Set[*santas]
-moves  = { '^' => 1i, 'v' => -1i, '<' => -1, '>' => 1 }
+moves = { '^' => 1i, 'v' => -1i, '<' => -1, '>' => 1 }
 
 File.open('input.txt').each_char.with_index do |char, index|
   santas[index % 2] += moves[char]
-  houses            << santas[index % 2]
+  houses << santas[index % 2]
 end
 
 puts houses.size
