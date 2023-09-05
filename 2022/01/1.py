4c6c10698307432b5b1e6477654f24fb19ccfdd2
Matthew Winiarczyk <matthew.winiarczyk@gmail.com>
2023-09-05 18:17:53 -0400

    Edit day 1, 2022

--- 2022/01/1.py
+++ 2022/01/1.py
@@ -5,9 +5,9 @@ with open("input.txt") as f:
     for line in f:
         line = line.rstrip()
         if line == "":
+            answer = max(calories, answer)
             calories = 0
         else:
             calories += int(line)
-            answer = max(calories, answer)
 
 print(answer)
