c81f3fddc288d047e17da3c59c71db01466c96c6
Matthew Winiarczyk <matthew.winiarczyk@gmail.com>
2023-10-04 17:31:19 -0400

    Add day 4, 2022

---
+++ 2022/04/2.py
@@ -0,0 +1,13 @@
+from re import findall
+
+answer = 0
+
+with open("input.txt") as f:
+    for line in f:
+        a1, a2, b1, b2 = [int(m) for m in findall("(\d+)", line)]
+        s1 = set(range(a1, a2+1))
+        s2 = set(range(b1, b2+1))
+        if s1.intersection(s2):
+            answer += 1
+
+print(answer)
