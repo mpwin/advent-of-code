f49587bbbc4f6647c4aa3614bc86650b2cabedb2
Mateusz Winiarczyk <mateusz.winiarczyk@tuta.com>
2025-12-18 18:47:30 +0100

    Add day 8, 2025

---
+++ 2025/08/2.py
@@ -0,0 +1,37 @@
+from itertools import combinations
+from math import dist
+
+with open("input.txt") as f:
+    boxes = tuple(
+        tuple(map(int, line.strip().split(',')))
+        for line in f
+    )
+    circuits = [{i} for i in range(len(boxes))]
+
+distances = {
+    (i, j): dist(boxes[i], boxes[j])
+    for i, j in combinations(range(len(boxes)), 2)
+}
+pairings = sorted(distances, key=distances.get)
+
+for box1, box2 in pairings:
+    circuit1 = None
+    circuit2 = None
+
+    for circuit in circuits:
+        if box1 in circuit:
+            circuit1 = circuit
+        if box2 in circuit:
+            circuit2 = circuit
+        if circuit1 and circuit2:
+            break
+
+    if circuit1 is not circuit2:
+        circuit1 |= circuit2
+        circuits.remove(circuit2)
+
+    if len(circuits) == 1:
+        answer = boxes[box1][0] * boxes[box2][0]
+        break
+
+print(answer)
